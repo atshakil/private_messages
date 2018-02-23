73a77a9135dcf061a4ed24e15e4be6cab9b0834f
Tahmid Shakil <at.shakil.92@gmail.com>
2018-02-23 18:44:42 +0700

    Authorization: View consistency

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -9,4 +9,8 @@ module MessagesHelper
     recipient = message.recipient
     recipient.full_name if recipient
   end
+
+  def authorized_edit_message_path message
+    link_to("Edit", edit_message_path(@message)) if can? :update, message
+  end
 end
